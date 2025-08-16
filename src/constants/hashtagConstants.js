53642d52f3e3c0bf566b0fc4e6c58d6ef059c6c2
yassinelaouni <yassineelaouni581@gmail.com>
2025-08-16 03:15:23 +0100

    remove constants folder

--- src/constants/hashtagConstants.js
+++
@@ -1,4 +0,0 @@
-// Index file that exports all hashtag-related functions
-export { getHashtagDescription } from './hashtagDescriptions'
-export { getHashtagImage } from './hashtagImages'
-export { getHashtagStats } from './hashtagStats'
